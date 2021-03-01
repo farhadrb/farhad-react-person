6e5aff4336695ff5bc7187ed4741c24caba31357
farhad rabiee <farhadrabie@yahoo.com>
2021-03-01 03:16:46 -0800

    first-commit-init

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,42 @@
+import React, { useContext,useEffect} from "react";
+import { Alert, Badge } from "react-bootstrap";
+import SimpleContext from "./../../context/SimpleContext";
+
+const Header = ({ appTitle }) => {
+
+    const context = useContext(SimpleContext);
+    const { persons } = context;
+
+    useEffect(()=>{
+      const timer = setTimeout(() => {
+
+        }, 1000);
+        return ()=>{
+            clearTimeout(timer)
+        }
+    },[persons])
+
+
+    let badgeStyle = "";
+
+    if (persons.length >= 3) badgeStyle = "success";
+    if (persons.length <= 2) badgeStyle = "warning";
+    if (persons.length <= 1) badgeStyle = "danger";
+
+    return (
+        <div>
+            <Alert variant="info">
+                <h2>{appTitle}</h2>
+            </Alert>
+            <Alert variant="light">
+                تعداد اشخاص{" "}
+                <Badge pill variant={badgeStyle}>
+                    {persons.length}
+                </Badge>{" "}
+                نفر می باشد
+            </Alert>
+        </div>
+    );
+};
+
+export default Header;
